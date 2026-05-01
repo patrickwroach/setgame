0e7e93ed4dc1eb9537595c7ebdc25c480e877922
Roach, Patrick <patrickroach@comcast.com>
2026-05-01 16:35:35 -0400

    feat: Update Firebase initialization to use environment variables and add dotenv dependency

--- migrate-users.js
+++ migrate-users.js
@@ -1,9 +1,13 @@
 // Script to reset users collection - delete all and recreate with UIDs from Firebase Auth
+require('dotenv').config({ path: '.env.local' });
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  })
 });
 
 const db = admin.firestore();
--- set-admin.js
+++ set-admin.js
@@ -1,11 +1,15 @@
 // Script to set a user as admin
 // Usage: node set-admin.js <email> <true|false>
 
+require('dotenv').config({ path: '.env.local' });
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  })
 });
 
 const db = admin.firestore();
